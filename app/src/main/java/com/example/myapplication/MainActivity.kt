111dcf592e342345661fb2c70853badff246b427
Dimov,Samuil S <samuildimov347@gmail.com>
2023-03-19 20:18:12 +0100

    Combined STT and TTS into one app
    STT- speech to text
    TTS- text to speech

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,30 +1,90 @@
 package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.speech.RecognizerIntent
 import android.speech.tts.TextToSpeech
-import android.widget.Button
-import android.widget.EditText
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
 import java.util.*
-
+import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
-   lateinit var tts: TextToSpeech
+    private val REQUEST_CODE_SPEECH_INPUT = 1
+    private lateinit var idIVMic: ImageView
+    private lateinit var idTVOutput: TextView
+    private lateinit var tts: TextToSpeech
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var b1 = findViewById<Button>(R.id.btn)
-        var e1 = findViewById<EditText>(R.id.edText)
+        // Find views manually
+        idIVMic = findViewById(R.id.idIVMic)
+        idTVOutput = findViewById(R.id.idTVOutput)
 
-        b1.setOnClickListener{
+        // Text to Speech
+        val b1 = findViewById<Button>(R.id.btn)
+        val e1 = findViewById<EditText>(R.id.edText)
+
+        b1.setOnClickListener {
             tts = TextToSpeech(applicationContext, TextToSpeech.OnInitListener {
-                if(it==TextToSpeech.SUCCESS){
+                if (it == TextToSpeech.SUCCESS) {
                     tts.language = Locale.US
                     tts.setSpeechRate(1.0f)
                     tts.speak(e1.text.toString(), TextToSpeech.QUEUE_ADD, null)
                 }
             })
         }
+
+        // Speech to Text
+        idIVMic.setOnClickListener {
+            val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+
+            intent.putExtra(
+                RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
+            )
+            //one can choose specific language but its on default right now
+            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
+            intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Speech to Text")
+            try {
+                startActivityForResult(intent, REQUEST_CODE_SPEECH_INPUT)
+            } catch (e: Exception) {
+                Toast.makeText(this, "" + e.message, Toast.LENGTH_LONG).show()
+            }
+        }
+
+        // Copy to clipboard
+        val idBtnCopy = findViewById<Button>(R.id.idBtnCopy)
+        idBtnCopy.setOnClickListener {
+            val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+            val clip = ClipData.newPlainText("output", idTVOutput.text.toString())
+            clipboard.setPrimaryClip(clip)
+            Toast.makeText(this, "Copied to clipboard", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        //if function is successful
+        if (requestCode == REQUEST_CODE_SPEECH_INPUT) {
+            if (resultCode == RESULT_OK && data != null) {
+                val res: ArrayList<String> =
+                    data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS) as ArrayList<String>
+
+                //display output speech to text in textview
+                idTVOutput.text = res[0]
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        tts.stop()
+        tts.shutdown()
     }
 }
